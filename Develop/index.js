e1588aa5e1f8a65232429530fb6b3fdae3244935
Micheal Ellis <mellis.eriecon@gmail.com>
2021-04-08 17:56:25 -0400

    added packages and package calls / some questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,34 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const MrkDwnGenerator = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter title for project')
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'Description',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
