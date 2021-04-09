43e72c1a07e959338eb439d8ba3a8cf81cf1d42d
michealjwellis <mellis.eriecon@gmail.com>
2021-04-09 16:20:45 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,34 +1,132 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const MrkDwnGenerator = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
+// const mrkDwnGenerator = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the name of your project? (Required)',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter title for project')
-                return false;
-            }
-        }
+        message: 'What is the name of your project? ',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide description of your project: '
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What gave you the idea to create this project: '
+    },
+    {
+        type: 'input',
+        name: 'codeUsed',
+        message: 'What coding languages did you use? (Seperate with commas)'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license was used for this README? ',
+        choices: ['Unlicense', 'Apache License 2.0', 'GNU General Public License v3.0',
+            'MIT License', 'BSD 2-Clause', 'BSD 3-Clause',
+            'Boost Software License 1.0', 'ISC License', 'IBM Public License Version 1.0','Pearl License']
+    },
+    {
+        type: 'list',
+        name: 'color',
+        message: 'What color would you like to make your license badge?',
+        choices: ['brightgreen','green','yellowgreen','yellow','orange','red','blue','blueviolet']
 
     },
     {
         type: 'input',
-        name: 'Description',
+        name: 'usage',
+        message: 'Please provide necessary information for usage of project! '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What necessary dependencies must be installed to run this app? '
+    },
+    {
+        type: 'input',
+        name: 'contributor',
+        message: 'Please advise on any contributions you have made: '
+    },
+    {
+        type: 'input',
+        name: 'test', 
+        message: 'Please advise on your test opporations performed for your project: '
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to add mentions to your README? ',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'mentions',
+        message: 'Please provide mentions: ',
+        when: ({confirmAbout}) => {
+            if(confirmAbout) {
+                return true;
+                
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmImage',
+        message: 'Would you like to add images to your README? ',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'images',
+        message: 'Please provide image link: ',
+        when: ({confirmImage}) => {
+            if (confirmImage) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-];
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+
+    fs.writeFile('../' + fileName, data, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log ('Successfully wrote: ' + fileName);
+    })
+ }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+        .then(function (data) {
+            writeToFile('README.md', generateMarkdown(data));
+        })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // return `# ![License: ${data.license}](https://img.shields.io/badge/license-${data.license}-${data.color}.svg)`
+} 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +14,36 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  
+  return `
+  # ${data.title}
+  * ![License: ${data.license}](https://img.shields.io/badge/license-${data.license}-${data.color}.svg)
 
+  ## Description
+
+  ${data.description}
+
+  ## Motivation
+
+  ${data.motivation}
+
+  ## Code Used
+
+  ${data.codeUsed}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
 `;
 }
+
 
 module.exports = generateMarkdown;
